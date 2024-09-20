17dc77cbc853bc788fa0e5356cf0a44b0581cb21
Brayan Yepes <abrayan.claye@gmail.com>
2024-09-20 11:46:57 -0500

    feat: add actions to buttons in heroes details

---
+++ src/heroes/presentation/molecules/HeroesActionsBtns.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+import { PencilIcon, TrashIcon, ArrowUturnLeftIcon } from '@heroicons/vue/24/outline';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+const goBack = () => router.push('/heroes');
+
+</script>
+
+<template>
+    <div class="flex gap-4 absolute -bottom-5 -right-5">
+        <button class="flex justify-center items-center w-10 h-10 rounded-full bg-indigo-500 text-white hover:bg-indigo-800">
+            <PencilIcon class="w-4 h-4" />
+        </button>
+        <button class="flex justify-center items-center w-10 h-10 bg-gray-400 rounded-full text-white hover:bg-gray-500">
+            <TrashIcon class="w-4 h-4" />
+        </button>
+        <button @click="goBack" class="flex justify-center items-center w-10 h-10 bg-gray-400 rounded-full text-white hover:bg-gray-500">
+            <ArrowUturnLeftIcon class="w-4 h-4" />
+        </button>
+    </div>
+</template>
--- src/heroes/presentation/templates/ViewHero.vue
+++ src/heroes/presentation/templates/ViewHero.vue
@@ -4,6 +4,7 @@ import { useGetHeroById } from '../hooks/useGetHeroById.hook';
 import { onMounted, ref } from 'vue';
 import Loading from '../../../common/presentation/atoms/Loading.vue';
 import { Hero } from '../../domain/Heroes.entity';
+import HeroesActionsBtns from '../molecules/HeroesActionsBtns.vue';
 
 const route = useRoute();
 const hero = ref<Hero | null>(null);
@@ -25,36 +26,13 @@ onMounted(async () => {
                 <img v-if="hero?.picture" :src="hero.picture" alt="Hero image" class="w-96 h-96">
             </div>
             <div class="w-full h-full border shadow-md rounded-lg p-10 flex flex-col justify-between relative">
-                <div class="flex justify-between items-center">
-                    <span class="font-bold text-gray-500">Agility</span>
-                    <p class="text-sm w-8 h-5 flex justify-center items-center text-white rounded-lg p-1 bg-pink-500">{{
-                        hero?.attributes.agility }}</p>
+                <div v-for="(value, key) in hero?.attributes" :key="key" class="flex justify-between items-center">
+                    <span class="font-bold text-gray-500 capitalize">{{ key }}</span>
+                    <p class="text-sm w-8 h-5 flex justify-center items-center text-white rounded-lg p-1 bg-pink-500">
+                        {{ value }}
+                    </p>
                 </div>
-                <div class="flex justify-between items-center">
-                    <span class="font-bold text-gray-500">Strength</span>
-                    <p class="text-sm w-8 h-5 flex justify-center items-center text-white rounded-lg p-1 bg-pink-500">{{
-                        hero?.attributes.strength }}</p>
-                </div>
-                <div class="flex justify-between items-center">
-                    <span class="font-bold text-gray-500">Weight</span>
-                    <p class="text-sm w-8 h-5 flex justify-center items-center text-white rounded-lg p-1 bg-pink-500">{{
-                        hero?.attributes.weight }}</p>
-                </div>
-                <div class="flex justify-between items-center">
-                    <span class="font-bold text-gray-500">Endurance</span>
-                    <p class="text-sm w-8 h-5 flex justify-center items-center text-white rounded-lg p-1 bg-pink-500">{{
-                        hero?.attributes.endurance }}</p>
-                </div>
-                <div class="flex justify-between items-center">
-                    <span class="font-bold text-gray-500">Charisma</span>
-                    <p class="text-sm w-8 h-5 flex justify-center items-center text-white rounded-lg p-1 bg-pink-500">{{
-                        hero?.attributes.charisma }}</p>
-                </div>
-                <div class="flex gap-4 absolute -bottom-5 -right-5">
-                    <button class="w-10 h-10 rounded-full bg-indigo-500 text-white hover:bg-indigo-800">E</button>
-                    <button class="w-10 h-10 bg-gray-200 rounded-full text-white hover:bg-gray-500">D</button>
-                    <button class="w-10 h-10 bg-gray-200 rounded-full text-white hover:bg-gray-500">V</button>
-                </div>
+                <HeroesActionsBtns />
             </div>
         </div>
     </div>
