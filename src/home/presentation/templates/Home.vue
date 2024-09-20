906525ffc519f0a463782a63f41141d74e54e526
Brayan Yepes <abrayan.claye@gmail.com>
2024-09-20 13:44:56 -0500

    feat: improve home

--- src/home/presentation/templates/Home.vue
+++ src/home/presentation/templates/Home.vue
@@ -1,3 +1,7 @@
 <template>
-  <h1>Home</h1>
+  <div class="mt-20 flex flex-col justify-center items-center space-y-4">
+    <h1 class="text-5xl text-transparent bg-clip-text bg-gradient-to-tr from-indigo-500 to-pink-500 font-bold">Welcome to the best heroes platform</h1>
+    <p class="text-gray-500 text-xl">Here you can select your favorite heroes and run a competition to see who is the best!</p>
+    <router-link to="/heroes" class="bg-gradient-to-tr from-indigo-500 to-pink-500 cursor-pointer text-white rounded-lg p-2 mt-4">Select heroes</router-link>
+  </div>
 </template>
